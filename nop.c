10eeb47ece6a6814baad9b361dc3216dbbb1a234
Felipe Olivera <135641220+Teby4@users.noreply.github.com>
2024-01-05 11:19:53 -0300

    nop error fix

--- nop.c
+++ nop.c
@@ -1,6 +1,14 @@
 #include"monty.h"
 
-void nop(stack_t *h, unsigned int a)
+/**
+ * nop - Doesnt do anything.
+ * @h: It's a pointer to the stack.
+ * @a: It's the number of line.
+ *
+ * Return: None.
+ */
+
+void nop(stack_t **h, unsigned int a)
 {
 	(void) *h;
 	(void) a;
