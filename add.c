120491b862d477ab948ce853c377b6b55dafc841
Felipe Olivera <135641220+Teby4@users.noreply.github.com>
2024-01-05 14:23:06 -0300

    add.c fix

--- add.c
+++ add.c
@@ -10,5 +10,20 @@
 
 void add(stack_t **h, unsigned int a)
 {
+	stack_t *current = *h;
+	unsigned int sum;
+
+	(void) a;
+
+	if (current == NULL || current->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_count);
+		exit(EXIT_FAILURE);
+	}
+
+	sum = curren->n + current->next->n;
+	current->next->n = sum;
+
+	pop(*h, line_count);
 
 }
