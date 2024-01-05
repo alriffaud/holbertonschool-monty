ad0015583152a5ea147696108735ba21ba2ce5db
Alberto Riffaud <alriffaud@google.com>
2024-01-05 07:22:34 -0800

    Modification of swap.c file.

--- swap.c
+++ swap.c
@@ -11,7 +11,7 @@
 void swap(stack_t **h, unsigned int a)
 {
 	stack_t *current = *h;
-	stack_t *aux = current->next->next;
+	stack_t *aux;
 
 	(void) a;
 	if (current == NULL || current->next == NULL)
@@ -19,7 +19,7 @@ void swap(stack_t **h, unsigned int a)
 		fprintf(stderr, "L%d: can't swap, stack too short\n", line_count);
 		exit(EXIT_FAILURE);
 	}
-	
+	aux = current->next->next;
 	*h = current->next;
 	if (current->next->next != NULL)
 	{
